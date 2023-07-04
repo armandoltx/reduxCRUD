836586224d45031ccd329d50a8411b9083e7ea11
Armando Ruiz <armando.ruiz@industrie.co>
2023-07-04 11:29:19 +1000

    564. Agregando react eouter dom; creando componente producto, nuevoproducto, editarproducto; agregando links al header

---
+++ src/components/NuevoProducto.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NuevoProducto = () => {
+  return (
+    <div>
+      /productos/nuevo
+    </div>
+  );
+};
+
+export default NuevoProducto;
---
+++ src/components/Productos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Productos = () => {
+  return (
+    <div>
+      Productos holAAA
+    </div>
+  );
+};
+
+export default Productos;
