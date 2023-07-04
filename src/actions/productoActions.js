3bc4448250d008450a482e07682237d53f76f386
Armando Ruiz <armando.ruiz@industrie.co>
2023-07-04 12:19:43 +1000

    569. Definiendo las primeras accionesd de Redux y creando los types

---
+++ src/actions/productoActions.js
@@ -0,0 +1,7 @@
+import {
+  AGREGAR_PRODUCTO,
+  AGREGAR_PRODUCTO_EXITO,
+  AGREGAR_PRODUCTO_ERROR,
+} from '../types';
+
+// esta funcion es la q se usa en la vista
---
+++ src/types/index.js
@@ -0,0 +1,4 @@
+// se usan en el action y el reducer y describen lo q pasa en la app
+export const AGREGAR_PRODUCTO = 'AGREGAR_PRODUCTO';
+export const AGREGAR_PRODUCTO_EXITO = 'AGREGAR_PRODUCTO_EXITO';
+export const AGREGAR_PRODUCTO_ERROR = 'AGREGAR_PRODUCTO_ERROR';
