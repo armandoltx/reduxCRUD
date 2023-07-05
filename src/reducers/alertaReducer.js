a0513e620b1fc646d5ddedffab78a34f10c489ee
Armando Ruiz <armando.ruiz@industrie.co>
2023-07-05 13:10:15 +1000

    592. Creando el reducer de Alerta; agregando los types y el state en el store

---
+++ src/reducers/alertaReducer.js
@@ -0,0 +1,14 @@
+import {MOSTRAR_ALERTA, OCULTAR_ALERTA} from '../types'
+
+// cada reducer tiene su propio state
+const initialState = {
+  alerta: null
+}
+
+export deafult function(state = initialState, actio) {
+  switch(action.type) {
+
+    default:
+      return state;
+  }
+}
